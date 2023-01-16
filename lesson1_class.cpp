22656a92c597531acf5878510cdbcca7551c8ac2
Angelinne <kayumi85@hotmail.com>
2023-01-16 22:59:52 +0900

    4

--- lesson1_class.cpp
+++ lesson1_class.cpp
@@ -6,19 +6,24 @@ private:
 		int x;
 		int y;
 public:
-	 int count();
-};
+	int getValue ()
+	{
+		return (this->x + this->y);
+	}
 
-int lesson1_class::count()
-{
-	this->x = 5;
-	this->y = 9;
-	return (this->x + this->y);
-}
+	void setValue (int value1, int value2)
+	{
+		this->x = value1;
+		this->y = value2;
+	}
+};
 
 int main(void)
 {
 	lesson1_class hoge;
+	int total;
 
-	std::cout << hoge.count() << std::endl;
+	hoge.setValue(10, 30);
+	total = hoge.getValue();
+	std::cout << total << std::endl;
 }
