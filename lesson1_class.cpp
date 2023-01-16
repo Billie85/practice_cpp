62a03e9c3bcb8e088772ade3bb2e678495d1d29c
Angelinne <kayumi85@hotmail.com>
2023-01-16 21:51:20 +0900

    2

--- lesson1_class.cpp
+++ lesson1_class.cpp
@@ -2,21 +2,23 @@
 
 class lesson1_class
 {
-	public:
+private:
 		int x;
 		int y;
-		int count_function()//関数をクラスの中に入れてあげることで、hoge.count_functionで呼んであげることができる。
-		{
-			return (x + y);
-		}
+public:
+	 int count();
 };
+
+int lesson1_class::count()
+{
+	this->x = 5;
+	this->y = 9;
+	return (this->x + this->y);
+}
 
 int main(void)
 {
-	int store;
 	lesson1_class hoge;
-	hoge.x = 4;
-	hoge.y = 5;
-	store = hoge.count_function();
-	std::cout << store << std::endl;
+
+	std::cout << hoge.count() << std::endl;
 }
